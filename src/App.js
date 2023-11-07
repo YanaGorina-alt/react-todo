0d61a22039d7f1c9ec5414e5a7a072af92d711a3
Yana Gorina <yagorina.ne@gmail.com>
2023-11-07 10:58:14 -0800

    feat: Use localStorage to save todoList, Introduce custom hook for managing todoList state, Use fragments

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import * as React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+const useSemiPersistentState = ()=> {
+
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList'))|| []);
+
+  React.useEffect(()=>{
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  },[todoList])
+
+  return [todoList, setTodoList]
+}
+
 function App() {
 
-  const [todoList, setTodoList] = React.useState([]);
-  
 
+  
+  const [todoList, setTodoList] = useSemiPersistentState();
   const addTodo = (newTodo)=>{
     setTodoList([...todoList, newTodo])
   }
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       <AddTodoForm addTodoForm = {addTodo}/>
       <TodoList todoList = {todoList}/>
-    </div>
+    </>
   );
 }
 
